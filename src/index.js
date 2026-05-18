89eac8f538e900c10768f79f539fc7c1d42c165f
Bui Phuoc Thinh <bphuocthinh@gmail.com>
2026-05-18 17:36:02 +0700

    create essential material and install sass

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(morgan('combined'))
+
+app.engine('hbs', exphbs.engine({
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, 'resources', 'views', 'layouts'),
+  partialsDir: path.join(__dirname, 'resources', 'views', 'partials'),
+  extname: '.hbs',
+}))
+
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources', 'views'))
+
+app.get('/', (req, res) => {
+  return res.render('home', { message: 'Hello World!' })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
