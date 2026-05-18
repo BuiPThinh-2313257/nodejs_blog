f27f095e0491f2ab0f7a0094e92c5b5cb1874dd8
Bui Phuoc Thinh <thinh.buiphuocthinh@hcmut.edu.vn>
2026-05-18 20:10:30 +0700

    update header and create search file

--- src/index.js
+++ src/index.js
@@ -20,8 +20,14 @@ app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources', 'views'))
 
 app.get('/', (req, res) => {
-  return res.render('home', { message: 'Hello World!' })
+  return res.render('home')
 })
+
+app.get('/search', (req, res) => {
+  console.log(req.query.q)
+  return res.render('search')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
